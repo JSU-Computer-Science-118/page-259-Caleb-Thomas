9eae57ec01feb01e6635095e471e440f75c3c1e6
Caleb-Thomas <117470273+Caleb-Thomas@users.noreply.github.com>
2022-11-11 21:09:35 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-//Caleb Thomas 11/9/22 J00986740
+// Caleb Thomas 11/9/22 J00986740
+// Chapter 4 program num 7
+
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -17,13 +19,27 @@ int main()
         cout << "There are two roots";
         cout << "Root1 = " << root1 << endl;
         cout << "Root2 = " << root2 << endl;
-        if (a > 0)
-            cout << "Has two real roots";
-
-        if (a < 0)
-            cout << "Has two complex roots";
     }
-    
-    
+    if (a > 0)
+       cout << "Has two real roots";
+    else 
+    {
+        root1 = (-b + sqrt((pow(b, 2) - 4 * a * c))) / (2 * a);
+        root2 = (-b - sqrt((pow(b, 2) - 4 * a * c))) / (2 * a);
+
+        cout << "There are two real roots";
+        cout << "Root1 = " << root1 << endl;
+        cout << "Root2 = " << root2 << endl;
+    }
+     if (a < 0)
+        cout << "Has two complex roots";
+     else 
+     {
+         root1 = (-b + sqrt((pow(b, 2) - 4 * a * c))) / (2 * a);
+         root2 = (-b - sqrt((pow(b, 2) - 4 * a * c))) / (2 * a);
+
+         cout << "There are two complex roots";
+         cout << "Root1 = " << root1 << endl;
+         cout << "Root2 = " << root2 << endl;
+     }
     return 0;
-}
