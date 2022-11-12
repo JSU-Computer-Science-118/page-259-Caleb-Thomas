61e162565c918ea7df45a682589c7ad426c446a8
Caleb-Thomas <117470273+Caleb-Thomas@users.noreply.github.com>
2022-11-11 21:43:29 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     double a, b, c, root1, root2;
-    cout << "To solve an qudratic equation, enter the coffeicents if a,b,c in this order";
+    cout << " To solve an qudratic equation, enter the coffeicents if a,b,c in this order ";
     cin >> a >> b >> c;
     if (a == 0)
         cout << " It is not a quadratic equation";
@@ -16,21 +16,21 @@ int main()
         root1 = (-b + sqrt((pow(b, 2) - 4 * a * c))) / (2 * a);
         root2 = (-b - sqrt((pow(b, 2) - 4 * a * c))) / (2 * a);
 
-        cout << "There are two roots";
-        cout << "Root1 = " << root1 << endl;
-        cout << "Root2 = " << root2 << endl;
+        cout << " There are two roots";
+        cout << " Root1 = " << root1 << endl;
+        cout << " Root2 = " << root2 << endl;
     }
     if (a > 0)
-       cout << "Has two real roots";
+       cout << " Has two real roots ";
     else 
     {
         root1 = (-b + sqrt((pow(b, 2) - 4 * a * c))) / (2 * a);
         root2 = (-b - sqrt((pow(b, 2) - 4 * a * c))) / (2 * a);
 
-        cout << "There are two real roots";
+        cout << " There are two real roots ";
         cin >> a >> b >> c
-        cout << "Root1 = " << root1 << endl;
-        cout << "Root2 = " << root2 << endl;
+        cout << " Root1 = " << root1 << endl;
+        cout << " Root2 = " << root2 << endl;
     }
      if (a < 0)
         cout << "Has two complex roots";
@@ -39,9 +39,8 @@ int main()
          root1 = (-b + sqrt((pow(b, 2) - 4 * a * c))) / (2 * a);
          root2 = (-b - sqrt((pow(b, 2) - 4 * a * c))) / (2 * a);
 
-         cout << "There are two complex roots";
-         cin >> a >> b >> c;
-         cout << "Root1 = " << root1 << endl;
-         cout << "Root2 = " << root2 << endl;
+         cout << " There are two complex roots "
+         cout << " Root1 = " << root1 << endl;
+         cout << " Root2 = " << root2 << endl;
      }
     return 0;
